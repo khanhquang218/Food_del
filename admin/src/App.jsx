48fe1cc1e5f605d865b5633db4f7275023a08d90
Khánh Quang <khanhquang213@gmail.com>
2024-06-12 20:28:25 +0700

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
+import Add from './pages/Add/Add';
 
 const App = () => {
-	return <div></div>;
+	return (
+		<div>
+			<Navbar />
+			<hr />
+			<div className="app-content">
+				<Sidebar />
+				<Routes>
+					<Route path="/add" element={<Add />} />
+					<Route path="/list" element={<List />} />
+					<Route path="/orders" element={<Orders />} />
+				</Routes>
+			</div>
+		</div>
+	);
 };
 
 export default App;
