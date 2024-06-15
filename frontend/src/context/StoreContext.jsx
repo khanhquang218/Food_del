e3b045aea60c57f60042a5b39412f58f77948ba2
Khanh Quang <khanhquang213@gmail.com>
2024-06-15 23:44:57 +0700

    update add to cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import { food_list } from '../assets/assets';
+import axios from 'axios';
 
 export const StoreContext = createContext(null);
 
@@ -7,17 +8,24 @@ const StoreContextProvider = (props) => {
 	const [cartItems, setCartItems] = useState({});
 	const url = 'http://localhost:4000';
 	const [token, setToken] = useState('');
+	const [food_list, setFoodList] = useState([]);
 
-	const addToCart = (itemId) => {
+	const addToCart = async (itemId) => {
 		if (!cartItems[itemId]) {
 			setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
 		} else {
 			setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
 		}
+		if (token) {
+			await axios.post(url + '/api/cart/add', { itemId }, { headers: { token } });
+		}
 	};
 
-	const removeFromCart = (itemId) => {
+	const removeFromCart = async (itemId) => {
 		setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+		if (token) {
+			await axios.post(url + '/api/cart/remove', { itemId }, { headers: { token } });
+		}
 	};
 
 	const getTotalCartAmount = () => {
@@ -31,10 +39,25 @@ const StoreContextProvider = (props) => {
 		return totalAmount;
 	};
 
+	const fetchFoodList = async () => {
+		const response = await axios.get(url + '/api/food/list');
+		setFoodList(response.data.data);
+	};
+
+	const localCartData = async (token) => {
+		const response = await axios.post(url + '/api/cart/get', {}, { headers: { token } });
+		setCartItems(response.data.cartData);
+	};
+
 	useEffect(() => {
-		if (localStorage.getItem('token')) {
-			setToken(localStorage.getItem('token'));
+		async function loadData() {
+			await fetchFoodList();
+			if (localStorage.getItem('token')) {
+				setToken(localStorage.getItem('token'));
+				await localCartData(localStorage.getItem('token'));
+			}
 		}
+		loadData();
 	}, []);
 
 	const contextValue = {
