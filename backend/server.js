2d222da0a622ed1ba4f1186f7647e8e5933a20c7
Khanh Quang <khanhquang213@gmail.com>
2024-06-15 12:49:40 +0700

    update admin

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // api endpoints
 app.use('/api/food', foodRouter);
-app.use('/image', express.static('uploads'));
+app.use('/images', express.static('uploads'));
 
 app.get('/', (req, res) => {
 	res.send('API Working');
