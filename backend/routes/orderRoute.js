392e41c8af34d5b0b485b3c4f691f817d5ae813c
Khanh Quang <khanhquang213@gmail.com>
2024-06-20 11:47:53 +0700

    update

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/place', authMiddleware, placeOrder);
 orderRouter.post('/verify', verifyOrder);
 orderRouter.post('/userorders', authMiddleware, userOrders);
+orderRouter.get('/list', listOrders);
 
 export default orderRouter;
